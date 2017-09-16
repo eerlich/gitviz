aa3309c69ecd06b4bfae179a7744f2ea157bc457
Eero Lihavainen <eeelliha@gmail.com>
2017-09-16 12:53:37 +0200

    Outline functionality

--- getgraphs.py
+++ getgraphs.py
@@ -3,10 +3,19 @@ import os
 
 datadir = "gitviz_data"
 files = os.listdir(datadir)
-cond = lambda x : x.find("master")>0
 
+cond = lambda x : x.find("master")>0
 masterfile = [x for x in files if cond(x)]
 rest = [x for x in files if not cond(x)]
 
+# read files
+# parse json
 
+# find latest commit b[T] from each branch (compare to input date)
+timestamp = sys.argv[1]
 
+# sys calls to compute 
+# git diff --shortstat b[T] master[T] and
+# git diff --shortstat b[T] (git merge-base b[T] master[T])
+# parse and output to a file graphs.json as array:
+# each element contains data (t,d1,d2) for one branch
