da8130dad3ebfed8193036156c5173492c786f52
Eero Lihavainen <eeelliha@gmail.com>
2017-09-16 18:37:22 +0200

    Start json writing

--- getgraphs.py
+++ getgraphs.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import subprocess 
 import json
+import re
 import time
 
-def get_files(datadir = "gitviz_data"):
+def get_files(datadir):
     files = os.listdir(datadir)
     files = [os.path.join(datadir, x) for x in files]
 
@@ -19,16 +21,47 @@ def parsefile(f):
     return out
 
 def latestcommit(jsob,ts):
-    [x['commit'] for x in jsob if int(x['timestamp'])<ts ][0]
+    return [x['commit'] for x in jsob if int(x['timestamp'])<ts ][0]
+
+def utf2ascii(data):
+    udata=data.decode("utf-8")
+    asciidata=udata.encode("ascii","ignore")
+
+def get_diff(a,b):
+    process = subprocess.Popen(["git", "diff", "--shortstat", a, b], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+
+    fc = re.match(".*(\d+) files changed", output)
+    i = re.match(".*(\d+) insertions", output)
+    d = re.match(".*(\d+) deletions", output)
+
+    inserts = i.group() if i else 0
+    deletions = d.group() if d else 0
+    out = inserts + deletions
+
+    return out
+
+def get_mergebase(a,b):
+    process = subprocess.Popen(["git", "merge-base", a, b], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    return output
+
+def writejson(res,files):
+    for k in range(len(res)):
+        jsval=json.dumps(res[k])
+        with open(files[k]) as f:
+            f.write(jsval)
 
 def main():
+    datadir = "gitviz_data"
+
     if(len(sys.argv)>1):
         # assume unix timestamp input
         ts = int(sys.argv[1])
     else:
         ts = time.time()
 
-    (f,mf) = get_files()
+    (f,mf) = get_files(datadir)
     # parse json from files
     js = [parsefile(x) for x in f]
     mjs = [parsefile(x) for x in mf]
@@ -36,13 +69,19 @@ def main():
     # find latest commit b[T] from each branch (compare to input date)
     commits=[latestcommit(x,ts) for x in js]
     mastercommit=latestcommit(mjs[0],ts)
+    mergebases = [get_mergebase(x,mastercommit) for x in commits]
 
-    # sys calls to compute 
+    cumdiffs = [get_diff(a,b) for (a,b) in zip(commits,mergebases)]
+    masterdiffs = [get_diff(a,mastercommit) for a in commits]
 
-    # git diff --shortstat b[T] master[T] and
-    # git diff --shortstat b[T] (git merge-base b[T] master[T])
-    # parse and output to a file graphs.json as array:
     # each element contains data (t,d1,d2) for one branch
+    result = [{"timestamp": ts, "cumdiff": a, "masterdiff": b} \
+            for (a,b) in zip(cumdiffs,masterdiffs)]
+
+    outfiles=[x[:-5]+"_graph.json" for x in f]
+
+    # output to a file graphs.json
+    writejson(result, outfiles)
 
 if __name__ == "__main__":
     main()
