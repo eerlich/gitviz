e0b7561bb4cc344f99c323690e7573f7a2798d71
Eero Lihavainen <eeelliha@gmail.com>
2017-09-16 10:11:40 +0200

    Add json output

--- getlogs.sh
+++ getlogs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
-branches=`git branch -a --sort=committerdate`
+datadir=gitviz_data
+mkdir -p $datadir
+branches=`git branch -a --sort=committerdate | tr \* " "`
 i=0
 maxi=5
 while read -r line; do
     i=$((i+1))
-    echo "foo $line"
+    fname=`echo ${line} | tr \/ _`.json
+    ./log2json.sh "$line" > "$datadir"/"$fname"
     if [ "$i" -gt "$maxi" ]; then break; fi
 done <<< "$branches"
 
